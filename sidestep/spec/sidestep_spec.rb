e9f87a6c38407ceae6b9632410bbf6e751f98088
Hash <takuya21hashimoto@gmail.com>
2012-08-09 05:24:53 +0900

    use around filter and Dir.mktmpdir to prepare temporary testing dir

--- sidestep/spec/sidestep_spec.rb
+++ sidestep/spec/sidestep_spec.rb
@@ -4,40 +4,49 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require
 
+require 'tmpdir'
+
 PLUGIN = 'sidestep'
 
 describe "#{PLUGIN}.vim" do
 
   ############### prepare data and Vimrunner ###############
 
-  def prepare_data(filename='data.in')
-    system "cat << __EOF__ > #{filename}
-hoge, test, fuga
-def method c, b, a
-(test, hoge, fuga)
-def mmm(test, fuga, hoge)
-def mmm(test, fuga, hoge) tuhng
-def mmm( test, fuga, hoge )
-__EOF__"
-  end
-
-  def clean_data(filename='data.in')
-    system "rm #{filename}"
+  def mkfilewith(str, filename='data.in')
+    File.open(filename, "w") do |f|
+      f.puts(<<-__EOF__)
+#{str}
+      __EOF__
+    end
   end
 
   before :all do
     @vim = Vimrunner.start
     @vim.add_plugin(File.expand_path('../../', __FILE__), "plugin/#{PLUGIN}.vim")
-    prepare_data
   end
 
   after :all do
     @vim.kill
-    clean_data
+  end
+
+  # make tmpdir with Dir.mktmpdir for vim test.
+  around do |example|
+    # tmpdir is like /private/var/folders/vl/kz6rxxx000gn/T/d20120809-7454..
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        @vim.command "cd #{dir}"
+        example.run
+      end
+    end
   end
 
   it "Vimrunner runs correctly" do
     @vim.class.should eq Vimrunner::Client
+  end
+
+  it 'around filter + tmpdir works correctly' do
+    mkfilewith("hoge", "test.in")
+    @vim.echo("filereadable(expand('test.in'))").should eq "1"
   end
 
   it 'loads plugin' do
@@ -45,9 +54,8 @@ __EOF__"
   end
 
   ############### plugin specific specs ###############
-
   it 'replace when the line has 3 items' do
-    # @vim.command "pwd"
+    mkfilewith("hoge, test, fuga")
   end
 
 end
