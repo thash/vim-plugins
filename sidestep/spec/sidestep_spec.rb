a6f7b58ceadc78a8b00347448b7d5279d1dd8dcd
Hash <takuya21hashimoto@gmail.com>
2013-07-07 02:47:47 +0900

    make RSpec testing available, then add some tests

--- sidestep/spec/sidestep_spec.rb
+++ sidestep/spec/sidestep_spec.rb
@@ -12,17 +12,25 @@ describe "#{PLUGIN}.vim" do
 
   ############### prepare data and Vimrunner ###############
 
-  def mkfilewith(str, filename='data.in')
+  def make_and_edit(str, filename='data.in')
+    # http://stackoverflow.com/questions/8565357/undo-all-changes-since-opening-buffer-in-vim
+    # undo all changes then delete buffer
+    @vim.normal("u1|u")
+    @vim.command("bdelete")
     File.open(filename, "w") do |f|
       f.puts(<<-__EOF__)
 #{str}
       __EOF__
     end
+    @vim.command("edit #{filename}")
   end
 
   before :all do
     @vim = Vimrunner.start
     @vim.add_plugin(File.expand_path('../../', __FILE__), "plugin/#{PLUGIN}.vim")
+    @sid = @vim.command('scriptnames').split("\n").find{|script|
+      script.index("#{PLUGIN}.vim")
+    }.scan(/(\d+):/)[0][0].to_i
   end
 
   after :all do
@@ -45,8 +53,8 @@ describe "#{PLUGIN}.vim" do
   end
 
   it 'around filter + tmpdir works correctly' do
-    mkfilewith("hoge", "test.in")
-    @vim.echo("filereadable(expand('test.in'))").should eq "1"
+    make_and_edit("hoge")
+    @vim.echo("filereadable(expand('data.in'))").should eq "1"
   end
 
   it 'loads plugin' do
@@ -55,9 +63,31 @@ describe "#{PLUGIN}.vim" do
 
   ############### plugin specific specs ###############
   it 'replace when the line has 3 items' do
-    mkfilewith("hoge, test, fuga")
+    make_and_edit("hoge, test, fuga")
+    @vim.normal("gg0")
+    @vim.command("call <SNR>#{@sid}_Sidestep('r')")
+    @vim.echo("getline('.')").should eq "test, hoge, fuga"
   end
 
+  describe 'with (double|single) quote' do
+    # [orig] (get*line('.'), 'aa,bbb')
+    #   [x]  ('aa, get*line('.'),bbb')
+    #   [o]  ('aa,bbb', get*line('.'))
+    it 'should detect quote-surrounded arguments' do
+      make_and_edit("(getline('.'), 'aa,bbb')")
+      @vim.normal("gg03l")
+      @vim.command("call <SNR>#{@sid}_Sidestep('r')")
+      @vim.echo("getline('.')").should eq "('aa,bbb', getline('.'))"
+    end
+
+    # [orig] "ho*ge, test, fuga"
+    #   [x]  test, "ho*ge, fuga"
+    #   [o]  "test, ho*ge, fuga"
+    it 'should work inside a quote' do
+      make_and_edit('"hoge, test, fuga"')
+      @vim.normal("gg02l")
+      @vim.command("call <SNR>#{@sid}_Sidestep('r')")
+      @vim.echo("getline('.')").should eq '"test, hoge, fuga"'
+    end
+  end
 end
-
-
